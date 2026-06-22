a462ddb7af48bf145a796ba355794ea5c1ee948c
Suvesh KUmar <suveshk2003@gmail.com>
2026-06-22 11:49:25 +0530

    karyakari

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,9 +15,9 @@ const Home = () => {
   { name: 'Prakash Kumar', role: 'Founder & President', roleHindi: 'संस्थापक एवं अध्यक्ष', image: '/images/prakash.png' },
   { name: 'Manish Tiwari', role: 'Vice President', roleHindi: 'उपाध्यक्ष', image: '/images/manish.png' },
   { name: 'Anshu Singh', role: 'Youth Wing Head', roleHindi: 'युवा प्रमुख', image: '/images/yuva.jpeg' },
+  { name: 'Gaurav Singh', role: 'Executive Member', roleHindi: 'कार्यकारी सदस्य', image: '/images/karyakari.jpg' },
   { name: 'Team Member 4', role: 'Treasurer', roleHindi: 'कोषाध्यक्ष', image: null },
   { name: 'Team Member 5', role: 'Joint Secretary', roleHindi: 'संयुक्त सचिव', image: null },
-  { name: 'Team Member 6', role: 'Media In-charge', roleHindi: 'सदमीडिया प्रभारी', image: null },
   { name: 'Team Member 7', role: 'General Secretary', roleHindi: 'महासचिव', image: null },
   { name: 'Team Member 8', role: 'Legal Advisor', roleHindi: 'कानूनी सलाहकार', image: null },
 ];
