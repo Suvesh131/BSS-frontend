7b413ea74db719fd5b1799d3f9cec19f219fe129
Suvesh KUmar <suveshk2003@gmail.com>
2026-08-24 20:46:51 +0530

    update

--- src/pages/Complaint.js
+++ src/pages/Complaint.js
@@ -232,6 +232,17 @@ const complaintData = [
     response: '',
     status: 'PENDING',
   },
+  {
+    slNo: 22,
+    date: '24/08/2026',
+    lastUpdated: '2026-08-24T12:00:00',
+    latterNo: 'BSS-034',
+    refNo: 'REF767253',
+    refLink: 'https://drive.google.com/file/d/1rge9cksaXe7LZ0NHC9gq12tpGKzjeXTL/view',
+    description: 'सोशल मीडिया पर जातिसूचक, धार्मिक एवं अश्लील/अपमानजनक वीडियो, पोस्ट एवं टिप्पणियों के माध्यम से सामाजिक सौहार्द बिगाड़ने के संबंध में शिकायत एवं उचित कानूनी कार्रवाई हेतु।',
+    response: '',
+    status: 'PENDING',
+  },
 ];
 
 const statusStyles = {
